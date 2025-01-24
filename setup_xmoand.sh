0db1387abbda6d94c282bea5963b6c43429a6f7f
IUH-Quis <92429927+DoanCongQui@users.noreply.github.com>
2025-01-24 12:29:08 +0700

    Update setup_xmoand.sh

--- setup_xmoand.sh
+++ setup_xmoand.sh
@@ -2,3 +2,28 @@
 
 # Downlaod xmoad 
 echo "Hello Setup Xmonad"
+sudo apt update -y && sudo apt upgrade -y 
+sudo apt install git libx11-dev libxft-dev libxinerama-dev libxrandr-dev libxss-dev g++ -y
+git clone https://github.com/xmonad/xmonad
+git clone https://github.com/xmonad/xmonad-contrib
+
+# Edit bashrc
+cat << 'EOF' | tee -a ~/.bashrc
+
+# set PATH so it includes user local bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
+EOF
+
+# Apply changes immediately
+source ~/.bashrc
+
+sudo apt install haskell-stack xmobar -y
+stack upgrade
+cd ~/.config/xmonad
+stack init && stack upgrade
+stack install 
+
+
